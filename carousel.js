a8bc2fd73a6aae36c2fcaa06cfb716b50adf669f
buriburipro <buriburipro@outlook.com>
2015-09-08 09:21:57 +0800

    add default parameters and functions
    set default parameters
    add functions to get and set customized parameters

--- carousel.js
+++ carousel.js
@@ -1,11 +1,40 @@
 ;(function($){
 	//Create a Class
-	function Carousel(ads){
-		alert(ads);
+	function Carousel(ad){
+		// alert(ads);
+		//save single node of ad
+		this.ad = ad;
+		// default parameters
+		this.setting = {
+			"width" : 1000,
+			"height" : 270,
+			"firstWidth" : 540,
+			"firstHeight" : 270,
+			"verticleAlign" : "middle",
+			"scale" : 0.9,
+			"speed" : 1000
+		}		
+		$.extend(this.setting, this.getCustomizedParameters());
+		// console.log(this.ad);
 	}
 	//write the prototype
 	Carousel.prototype = {
-
+		//set parameters
+		setParameters:function(){
+			this.ad.css({
+				width : this.setting.width,
+				height : this.setting.height
+			});
+		},
+		//get customized parameters
+		getCustomizedParameters:function(){
+			var setting = this.ad.attr("data-setting");
+			if(setting && setting != ""){
+				return $.parseJSON(setting);
+			}else{
+				return {};
+			}
+		}
 	}
 	//initiation
 	Carousel.init = function(ads){
