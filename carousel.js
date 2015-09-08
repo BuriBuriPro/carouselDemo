290a8d0d1a4a93c958d307c9653adcc3d01de2e3
buriburipro <buriburipro@outlook.com>
2015-09-08 09:43:42 +0800

    add function
    add function to set parameters

--- carousel.js
+++ carousel.js
@@ -2,8 +2,16 @@
 	//Create a Class
 	function Carousel(ad){
 		// alert(ads);
-		//save single node of ad
-		this.ad = ad;
+		//save single node of ad, adList, btnPrev, btnNext
+		this.ad = ad;		
+		this.adList = ad.find("ul.adList")
+		this.btnPrev = ad.find("div.btnPrev");
+		this.btnNext = ad.find("div.btnNext");
+		//save nodes of adItems
+		this.adItems = ad.find("li.adItem");
+		//save firstItem and lastItem
+		this.firstItem = this.adItems.first();
+		this.lastItem = this.adItems.last();
 		// default parameters
 		this.setting = {
 			"width" : 1000,
@@ -15,6 +23,7 @@
 			"speed" : 1000
 		}		
 		$.extend(this.setting, this.getCustomizedParameters());
+		this.setParameters();
 		// console.log(this.ad);
 	}
 	//write the prototype
@@ -25,6 +34,24 @@
 				width : this.setting.width,
 				height : this.setting.height
 			});
+			this.adList.css({
+				width : this.setting.width,
+				height : this.setting.height
+			});
+			//calculate the width of button
+			var w = (this.setting.width - this.setting.firstWidth) / 2;
+			this.btnPrev.css({
+				width : w,
+				height : this.setting.height
+			});
+			this.btnNext.css({
+				width : w,
+				height : this.setting.height
+			});
+			this.firstItem.css({
+				width : this.setting.firstWidth,
+				height : this.setting.firstHeight
+			})
 		},
 		//get customized parameters
 		getCustomizedParameters:function(){
